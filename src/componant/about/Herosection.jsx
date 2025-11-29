197982461f9c04c40872cda5a9a006748bb10ec7
sabbirahmed32 <mdsabbirmdjoni@gmail.com>
2025-11-29 20:58:37 +0600

    about section

--- src/componant/about/Herosection.jsx
+++ src/componant/about/Herosection.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import "../home/Hro.css"
 function Herosection() {
+
+    const aboutH = ()=> {
+    const aboutSection = document.getElementById('abouutN');
+    aboutSection.innerText=('Hello World');
+    }
+
+
   return (
-    <div>About hrrr</div>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 m-auto">
+            <h3 id='abouutN' className='pt-5'>About Section</h3>
+            <button onClick={aboutH} className='explore-btn'>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
