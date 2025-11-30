1ecafdadb11c52c325164fd8e640ac846fbb6447
sabbirahmed32 <mdsabbirmdjoni@gmail.com>
2025-11-30 21:22:21 +0600

    object

--- src/componant/About.jsx
+++ src/componant/About.jsx
@@ -2,9 +2,15 @@
 import React from "react"
 import Herosection from "./about/Herosection"
 function About(){
+    const product = {
+        name:'Smart Gaget',
+        price:'200'
+
+    }
+
     return (
         <div>
-            <Herosection></Herosection>
+            <Herosection Aproduct ={product}></Herosection>
         </div>
     )
 }
--- src/componant/about/Herosection.jsx
+++ src/componant/about/Herosection.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "../home/Hro.css"
-function Herosection() {
+function Herosection(props) {
 
     const aboutH = ()=> {
     const aboutSection = document.getElementById('abouutN');
     aboutSection.innerText=('Hello World');
     }
 
+    console.log(props)
 
   return (
     <div>
@@ -15,6 +16,13 @@ function Herosection() {
           <div className="col-md-6 m-auto">
             <h3 id='abouutN' className='pt-5'>About Section</h3>
             <button onClick={aboutH} className='explore-btn'>Submit</button>
+          </div>
+          <div className="col-md-6 mt-5">
+            <div className='Aproduct'>
+              <h5>Product</h5>
+              <h5>Name: {props.Aproduct.name}</h5>
+              <h5>Price: {props.Aproduct.price}</h5>
+            </div>
           </div>
         </div>
       </div>
