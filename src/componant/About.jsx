03304a0272c390bde9456d05fa2e1b222ec8377b
sabbirahmed32 <mdsabbirmdjoni@gmail.com>
2025-12-03 22:46:56 +0600

    Arrayes

--- src/componant/About.jsx
+++ src/componant/About.jsx
@@ -8,9 +8,25 @@ function About(){
 
     }
 
+    const arrays =["Rohim", "Korhim", "Habu", "Mondol"]
+
+    const producted = [
+
+        {name:'Nokia-50', price: '5000'},
+        {name: 'Samsung-A16 pro', price: '10000'},
+        {name: 'Oppo-A17 Pro max', price: '20000'},
+        
+    ];
+
     return (
         <div>
-            <Herosection Aproduct ={product}></Herosection>
+            <Herosection 
+            Aproduct ={product}
+            arrays ={arrays}
+            producted ={producted}
+            >
+
+            </Herosection>
         </div>
     )
 }
--- src/componant/about/Herosection.jsx
+++ src/componant/about/Herosection.jsx
@@ -7,7 +7,7 @@ function Herosection(props) {
     aboutSection.innerText=('Hello World');
     }
 
-    console.log(props)
+
 
   return (
     <div>
@@ -22,6 +22,22 @@ function Herosection(props) {
               <h5>Product</h5>
               <h5>Name: {props.Aproduct.name}</h5>
               <h5>Price: {props.Aproduct.price}</h5>
+            </div>
+          </div>
+          <div className="col-md-6 mt-5">
+            <div>
+              <h5>Name: {props.arrays[1]}</h5>
+              <h5>Price: </h5>
+            </div>
+          </div>
+          <div className="col-md-6 mt-5">
+            <div>
+           
+              <h5><span>Name: {props.producted[0].name}, <span>Price: {props.producted[0].price}</span></span></h5>
+
+              <h5><span>Name: {props.producted[1].name}, <span>Price: {props.producted[1].price}</span></span></h5>
+              <h5><span>Name: {props.producted[2].name}, <span>Price: {props.producted[2].price}</span></span></h5>
+              
             </div>
           </div>
         </div>
